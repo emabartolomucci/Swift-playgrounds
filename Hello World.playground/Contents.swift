77619600d2cfa5680b03edff379ae472a0c5a46b
Emanuele Bartolomucci <e@emanuelebartolomucci.com>
2015-12-03 21:03:22 +0000

    Added strings

--- Hello World.playground/Contents.swift
+++ Hello World.playground/Contents.swift
@@ -3,10 +3,16 @@
 import UIKit
 
 
+// Smple values
+
 let implicitInteger = 23
-
 let implicitDouble = 1.4142
-
 let explicitDouble : Double = 42.00
-
 let explicitFloat: Float = 4
+
+
+// Strings
+
+let label = "The width is "
+let width = 80
+let widthLabel = label + String(width)
