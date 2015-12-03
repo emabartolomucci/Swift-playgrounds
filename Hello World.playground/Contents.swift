b8256c9595f826ea5023504893f55524aa38da81
Emanuele Bartolomucci <emabartolomucci@MBP.local>
2015-12-03 20:56:06 +0000

    Folder cleaning

---
+++ Hello World.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let implicitInteger = 23
+
+let implicitDouble = 1.4142
+
+let explicitDouble : Double = 14.000
+
+let explicitFloat: Float = 4
+
