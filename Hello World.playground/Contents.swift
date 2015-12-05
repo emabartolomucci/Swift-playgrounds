a6f6e1566ccbe45163ba5ceb016b450414644643
Emanuele Bartolomucci <e@emanuelebartolomucci.com>
2015-12-05 23:00:54 +0000

    Value conversion for strings

--- Hello World.playground/Contents.swift
+++ Hello World.playground/Contents.swift
@@ -12,10 +12,19 @@ let explicitFloat: Float = 4
 
 
 // Strings
+//
+// Values are never implicitly converted to another type. If you need to convert a value to a different type, explicitly make an instance of the desired type.
 
 let label = "The width is "
 let width = 80
 let widthLabel = label + String(width)
+
+// There’s an even simpler way to include values in strings: Write the value in parentheses, and write a backslash (\) before the parentheses. For example:
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
 
 
 //Increment/decrement operators
